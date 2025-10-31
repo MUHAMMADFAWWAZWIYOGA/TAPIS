ca850c9e022d6c178e10dd0d65fe27b17e9604e5
Ahmad <ahmadmuhaiminkamil90@gmail.com>
2025-10-31 15:45:33 +0700

    Fixed some issues

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,26 @@ Route::get('/laporan', function () {
     return Inertia::render('Laporan');
 })->name('laporan');
 
-// route dashboard (hanya bisa diakses setelah login)
+// route publik untuk pemantauan (tanpa auth)
+Route::get('/pemantauan', function () {
+    return Inertia::render('Pemantauan');
+})->name('pemantauan');
+
+// route dashboard (sementara tanpa auth)
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
-// route profile (auth)
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// route publik untuk profile    (tanpa auth)
+Route::get('/profile', function () {
+    return Inertia::render('Profile');
+})->name('profile');
+
+// // route profile (hanya untuk user login)
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 
 require __DIR__.'/auth.php';
